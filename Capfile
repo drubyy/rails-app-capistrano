8f9f1545d7563523b15f9912e99dc383f428cdd9
duong.dev0410 <tran.thai.duong@sun-asterisk.com>
2021-09-27 19:18:35 +0700

    init capistrano

---
+++ Capfile
@@ -0,0 +1,20 @@
+# Load DSL and set up stages
+require "capistrano/setup"
+
+# Include default deployment tasks
+require "capistrano/deploy"
+
+
+
+require "capistrano/scm/git"
+install_plugin Capistrano::SCM::Git
+
+require 'capistrano/rails'
+require 'capistrano/passenger'
+require 'capistrano/rbenv'
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.7.2'
+
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
+Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,14 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+set :application, "deploy_with_capistrano"
+set :repo_url, "https://github.com/thaiduong99/deploy_with_capistrano.git"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
